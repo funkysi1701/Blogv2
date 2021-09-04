e1cc30ecaf04d7f406209cd0f6575119e3045f8c
Simon Foster <funkysi1701@gmail.com>
2021-09-04 22:15:07 +0100

    style tweaks

---
+++ Blog/Components/Document.razor.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace Blog.Components
+{
+    public class Document : ComponentBase, IDisposable
+    {
+        [Inject]
+        protected IJSRuntime JsRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && NavigationManager != null)
+                NavigationManager.LocationChanged -= LocationChanged;
+        }
+
+        protected override void OnInitialized()
+        {
+            if (NavigationManager != null)
+            {
+                NavigationManager.LocationChanged += LocationChanged;
+            }
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (!firstRender) return;
+            if (NavigationManager != null)
+            {
+                await SetTitle(new Uri(NavigationManager.Uri)).ConfigureAwait(false);
+            }
+        }
+
+        private async Task SetTitle(Uri uri)
+        {
+            var pageName = uri.AbsolutePath.Replace("/", "-", StringComparison.CurrentCultureIgnoreCase);
+            if (JsRuntime != null && pageName != "-")
+            {
+                await JsRuntime.InvokeVoidAsync("JsFunctions.setDocumentTitle", PageTitleGenerator.Create(pageName)).ConfigureAwait(false);
+            }
+        }
+
+        private async void LocationChanged(object sender, LocationChangedEventArgs e)
+        {
+            await SetTitle(new Uri(e.Location));
+        }
+    }
+}
---
+++ Blog/Components/PageTitleGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Blog.Components
+{
+    public static class PageTitleGenerator
+    {
+        public static string Create(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(title));
+
+            title = title.Replace('-', ' ');
+            if (title.Contains("posts"))
+            {
+                title = title.Replace("posts", "");
+                title = title[0..^4];
+            }
+
+            string pageTitle = title switch
+            {
+                "/" => string.Empty,
+                _ => $"{title}",
+            };
+            return pageTitle + " - Funky Si's Tech Talk";
+        }
+    }
+}
