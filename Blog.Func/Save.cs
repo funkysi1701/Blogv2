7066d2181dd8baf662807c6e7210e2e0a33a85d9
Simon Foster <funkysi1701@gmail.com>
2021-09-23 16:47:30 +0100

    save data once an hour

---
+++ Blog.Func/Save.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+
+namespace Blog.Func
+{
+    public class Save
+    {
+        [FunctionName("SaveData")]
+        public static void Run([TimerTrigger("0 59 * * * *")] TimerInfo myTimer, ILogger log, ExecutionContext context)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var client = new HttpClient();
+            log.LogInformation($"URL: {config.GetValue<string>("URL")}");
+            client.GetAsync(config.GetValue<string>("URL"));
+        }
+    }
+}
