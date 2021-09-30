84e4b1bce51cb355a726bd5cf19b2c95ab704a1b
Simon Foster <funkysi1701@gmail.com>
2021-09-30 16:14:55 +0100

    tweak

--- Blog.Func/Save.cs
+++ Blog.Func/Save.cs
@@ -9,7 +9,6 @@ namespace Blog.Func
     public class Save
     {
         private readonly TwitterService twitterService;
-        private readonly TwitterServiceV2 twitterService;
         private readonly PowerService powerService;
         private readonly GithubService githubService;
         private readonly DevToService devToService;
