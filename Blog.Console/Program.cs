28a092bf8f92ced398e729fa3287233174db728e
Simon Foster <funkysi1701@gmail.com>
2021-10-03 09:17:47 +0100

    tweak

--- Blog.Console/Program.cs
+++ Blog.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Core;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Cosmos.Fluent;
 using Microsoft.Extensions.Configuration;
 
@@ -15,7 +16,7 @@ var database = cosmosClient.GetDatabase(config["DatabaseName"]);
 var container = database.GetContainer(config["ContainerName"]);
 
 var listofdates = new List<DateTime?>();
-var totaln = container.GetItemLinqQueryable<Metric>(true).Where(x => x.Type >= (int)MetricType.Gas).ToList();
+var totaln = container.GetItemLinqQueryable<Metric>(true, null, new QueryRequestOptions { MaxItemCount = -1 }).Where(x => x.Type >= (int)MetricType.Gas).ToList();
 for (int i = 1; i < 13; i++)
 {
     for (int j = 1; j < 32; j++)
@@ -32,7 +33,7 @@ for (int i = 1; i < 13; i++)
             }
             else
             {
-                await container.DeleteItemAsync<Metric>(item.id.ToString(), new Microsoft.Azure.Cosmos.PartitionKey(item.PartitionKey));
+                await container.DeleteItemAsync<Metric>(item.id.ToString(), new PartitionKey(item.PartitionKey));
             }
         }
     }
@@ -42,7 +43,7 @@ for (int i = 1; i < 13; i++)
 
 for (int i = 0; i < (int)MetricType.Electricity + 1; i++)
 {
-    var m = container.GetItemLinqQueryable<Metric>(true).Where(x => x.Type == i).ToList();
+    var m = container.GetItemLinqQueryable<Metric>(true, null, new QueryRequestOptions { MaxItemCount = -1 }).Where(x => x.Type == i).ToList();
     foreach (var item in m)
     {
         if (string.IsNullOrEmpty(item.Username))
