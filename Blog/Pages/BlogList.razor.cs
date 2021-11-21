9c5aa7705181370b9377da6762772a898e392876
Simon Foster <funkysi1701@gmail.com>
2021-11-21 10:14:13 +0000

    update

---
+++ Blog/Pages/BlogList.razor.cs
@@ -0,0 +1,28 @@
+﻿using Blog.Core;
+using Blog.Data.Services;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Blog.Pages
+{
+    public class BlogListBase : ComponentBase
+    {
+        [Inject] BlogService BlogService { get; set; }
+        protected List<BlogPosts> blogs = new();
+
+        protected override void OnInitialized()
+        {
+            _ = new Timer(new TimerCallback(_ =>
+            {
+                InvokeAsync(async () =>
+                {
+                    blogs = await BlogService.GetBlogsAsync();
+                    blogs = blogs.Where(x => x.Published).ToList();
+                    StateHasChanged();
+                });
+            }), null, 1000, 5000);
+        }
+    }
+}
