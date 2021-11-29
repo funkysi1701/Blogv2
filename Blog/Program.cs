6b1c321b90d5f397001b16db1ac6f94fbded608b
Simon Foster <funkysi1701@gmail.com>
2021-11-29 21:35:58 +0000

    update titles

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,4 +1,5 @@
 using Blog.Data.Services;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,7 +14,7 @@ namespace Blog
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
+            builder.RootComponents.Add<HeadOutlet>("head::after");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<BlogService>();
 
