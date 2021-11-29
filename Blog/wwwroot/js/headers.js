32b69ad86fb6cefeb904437010e179d20909eed0
Simon Foster <simon@funkysi1701.com>
2021-11-29 21:45:39 +0000

    update titles (#51)

--- Blog/wwwroot/js/headers.js
+++ Blog/wwwroot/js/headers.js
@@ -1,9 +1,6 @@
 ﻿var JsFunctions = window.JsFunctions || {};
 
 JsFunctions = {
-    setDocumentTitle: function (title) {
-        document.title = title;
-    },
     offset: function offset() {
         return new Date().getTimezoneOffset() / 60;
     }
