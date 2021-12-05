0240f4eddc8deff72840fc957f5202f2fae5fe98
Simon Foster <simon@funkysi1701.com>
2021-12-05 09:22:01 +0000

    redirect (#56)

--- Blog/Pages/BlogPost.razor.cs
+++ Blog/Pages/BlogPost.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Blog.Core;
 using Blog.Data.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Blog.Pages
     public class BlogPostBase : ComponentBase
     {
         [Inject] private BlogService BlogService { get; set; }
+        [Inject] private IHttpContextAccessor HttpContextAccessor { get; set; }
 
         protected BlogPosts thisblog;
         protected BlogPostsSingle thisblogsingle;
@@ -21,7 +23,13 @@ namespace Blog.Pages
         {
             List<BlogPosts> blogs = await BlogService.GetBlogsAsync(200);
 
-            thisblog = blogs.FirstOrDefault(x => x.Slug == Slug && x.Published);
+            var url = HttpContextAccessor.HttpContext.Request.Path.Value;
+            if (url.Contains("lone-developer-to-senior-developer-my-2021-story-c3d"))
+            {
+                HttpContextAccessor.HttpContext.Response.Redirect("/posts/lone -developer-to-senior-developer-my-2021-story-3g0a"); 
+            }
+
+                thisblog = blogs.FirstOrDefault(x => x.Slug == Slug && x.Published);
             if (thisblog != null)
             {
                 thisblogsingle = await BlogService.GetBlogPostAsync(thisblog.Id);
