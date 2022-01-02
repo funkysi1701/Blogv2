b2818968a79f7fb763de941b41b119bdc8eb6af6
Simon Foster <funkysi1701@gmail.com>
2022-01-02 22:05:13 +0000

    update octopus api

---
+++ ImpSoft.OctopusEnergy.Api/GspException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ImpSoft.OctopusEnergy.Api
+{
+    public class GspException : ApplicationException
+    {
+        public GspException()
+        {
+        }
+
+        public GspException(string message) : base(message)
+        {
+        }
+
+        public GspException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
