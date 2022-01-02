d5820ed708dcf64edd78a844de35314d8b6df01f
Simon Foster <simon@funkysi1701.com>
2022-01-02 00:31:56 +0000

    fix axis of charts (#81)
    * fix axis of charts
    
    * tweak rawdata bits

--- Blog/Components/Line.razor.cs
+++ Blog/Components/Line.razor.cs
@@ -35,6 +35,10 @@ namespace Blog.Components
         [Parameter]
         public MyChartType Day { get; set; }
 
+        protected LineDataset<TimeTuple<decimal>> Set;
+
+        protected LineDataset<TimeTuple<decimal>> PrevSet;
+
         protected override void OnInitialized()
         {
             if (Day == MyChartType.Hourly)
@@ -64,7 +68,7 @@ namespace Blog.Components
                                     Ticks = new TimeTicks
                                     {
                                         Source = TickSource.Auto,
-                                        Reverse = true
+                                        Reverse = false
                                     },
                                     Time = new TimeOptions
                                     {
@@ -111,7 +115,7 @@ namespace Blog.Components
                                     Ticks = new TimeTicks
                                     {
                                         Source = TickSource.Auto,
-                                        Reverse = true
+                                        Reverse = false
                                     },
                                     Time = new TimeOptions
                                     {
@@ -158,7 +162,7 @@ namespace Blog.Components
                                     Ticks = new TimeTicks
                                     {
                                         Source = TickSource.Auto,
-                                        Reverse = true
+                                        Reverse = false
                                     },
                                     Time = new TimeOptions
                                     {
@@ -179,7 +183,7 @@ namespace Blog.Components
                 };
             }
 
-            var Set = new LineDataset<TimeTuple<decimal>>
+            Set = new LineDataset<TimeTuple<decimal>>
             {
                 BackgroundColor = ColorUtil.FromDrawingColor(Color.Blue),
                 BorderColor = ColorUtil.FromDrawingColor(Color.Blue),
@@ -192,7 +196,7 @@ namespace Blog.Components
                 Label = "Current"
             };
 
-            var PrevSet = new LineDataset<TimeTuple<decimal>>
+            PrevSet = new LineDataset<TimeTuple<decimal>>
             {
                 BackgroundColor = ColorUtil.FromDrawingColor(Color.LightBlue),
                 BorderDash = new int[] { 10, 5 },
