32ed9921d8b5727ccc254b8abed33b1e04b64ad3
Simon Foster <funkysi1701@gmail.com>
2022-01-07 20:55:44 +0000

    test

--- Blog/Components/Line.razor.cs
+++ Blog/Components/Line.razor.cs
@@ -15,6 +15,9 @@ using System.Linq;
 
 namespace Blog.Components
 {
+    /// <summary>
+    ///
+    /// </summary>
     public class LineBase : ComponentBase
     {
         protected LineConfig _config;
