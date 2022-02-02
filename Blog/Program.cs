63b2f0f94dac1f2499cf80494a712a903704e81e
Simon Foster <funkysi1701@gmail.com>
2022-02-02 22:01:00 +0000

    update for when hugo goes live

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -18,7 +18,6 @@ namespace Blog
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<BlogService>();
 
-            builder.Services.AddSingleton<AppVersionInfo>();
             await builder.Build().RunAsync();
         }
     }
