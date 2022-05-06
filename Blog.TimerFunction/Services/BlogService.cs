932ea4b1de90ba231f46d08f0f799f11658c8d24
Simon Foster <funkysi1701@gmail.com>
2022-05-06 07:42:19 +0100

    fix??

--- Blog.TimerFunction/Services/BlogService.cs
+++ Blog.TimerFunction/Services/BlogService.cs
@@ -2,11 +2,7 @@
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
-using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Xml.XPath;
@@ -32,46 +28,20 @@ namespace Blog.TimerFunction.Services
                 .Load(url)
                 .XPathSelectElements("//item")
                 .Count();
+            log.LogInformation($"{count} posts found");
             await Chart.SaveData(count, (int)MetricType.Blog, Configuration.GetValue<string>("Username1"));
         }
 
         public async Task GetOldBlogCount(ILogger log)
         {
             var url = Configuration.GetValue<string>("OldRSSFeed");
-            var content = await DownloadData(url, log);
-            if (content != null)
-            {
-                await File.WriteAllBytesAsync($"C:\\local\\Temp\\file.xml", content);
-                log.LogInformation("File Downloaded");
-                var count = XDocument
-                .Load("C:\\local\\Temp\\file.xml")
+
+            var count = XDocument
+                .Load(url)
                 .XPathSelectElements("//item")
                 .Count();
-                log.LogInformation($"{count} posts found");
-                await Chart.SaveData(count, (int)MetricType.OldBlog, Configuration.GetValue<string>("Username1"));
-            }
-            else
-            {
-                log.LogError("Download Failed");
-            }
-        }
-
-        public static async Task<byte[]> DownloadData(string url, ILogger log)
-        {
-            try
-            {
-                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls12 | SecurityProtocolType.Tls13 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
-                using var client = new HttpClient();
-                using var result = await client.GetAsync(url);
-                return result.IsSuccessStatusCode ? await result.Content.ReadAsByteArrayAsync() : null;
-            }
-            catch (Exception e)
-            {
-                log.LogError(e.Message);
-                log.LogError(e.InnerException?.Message);
-                log.LogError(e, "Error");
-                return null;
-            }
+            log.LogInformation($"{count} posts found");
+            await Chart.SaveData(count, (int)MetricType.OldBlog, Configuration.GetValue<string>("Username1"));
         }
     }
 }
